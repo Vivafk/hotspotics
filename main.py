f406ba49fd6dee30401f27318126e4e1156264a7
Vivafk <karen.fayzrakhmanov@gmail.com>
2024-03-29 19:33:41 +0500

    Удаление старых файлов, переработка парсера

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ client.start()
 channel_username = 'https://t.me/bdufstecru'
 channel_entity = client.get_entity(channel_username)
 
-# Скачивание постов и сохранение в файл JSON
-posts = client.get_messages(channel_entity, limit=10)  # Указать нужное количество постов
-posts_json = [post.to_dict() for post in posts]
-with open('posts.json', 'w', encoding='utf-8') as f:
-    json.dump(posts_json, f, ensure_ascii=False, indent=4)
+###  Не работает  ###
+# # Скачивание постов и сохранение в файл JSON
+# posts = client.get_messages(channel_entity, limit=10)  # Указать нужное количество постов
+# posts_json = [post.to_dict() for post in posts]
+# with open('posts.json', 'w', encoding='utf-8') as f:
+#     json.dump(posts_json, f, ensure_ascii=False, indent=4)
 
 # Закрытие клиента Telegram
 client.disconnect()
