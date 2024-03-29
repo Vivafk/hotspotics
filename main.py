4ca104524d8e3c7cf6af741f5d13d79e23104ab6
Vivafk <karen.fayzrakhmanov@gmail.com>
2024-03-29 13:51:35 +0500

    Скелет парсера

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+from telethon.sync import TelegramClient
+import json
 
-print('Check work of git')
+api_id = 'your_api_id'
+api_hash = 'your_api_hash'
+
+username = 'your_username'
+password = 'your_password'
+
+client = TelegramClient(username, api_id, api_hash)
+client.start()
+
+# Идентификатор канала, из которого хотите скачать посты
+channel_username = 'channel_username'
+channel_entity = client.get_entity(channel_username)
+
+# Скачивание постов и сохранение в файл JSON
+posts = client.get_messages(channel_entity, limit=10)  # Указать нужное количество постов
+posts_json = [post.to_dict() for post in posts]
+with open('posts.json', 'w', encoding='utf-8') as f:
+    json.dump(posts_json, f, ensure_ascii=False, indent=4)
+
+# Закрытие клиента Telegram
+client.disconnect()
