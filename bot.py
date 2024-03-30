4c3bc6d6bf75276d8963ace65e63e7197f0091ce
Vivafk <karen.fayzrakhmanov@gmail.com>
2024-03-30 05:06:09 +0500

    Удаление ненужных файлов; авторизация в боте, кнопки

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 
 userdata = list(open('data.txt', 'r'))
 print(userdata)
@@ -7,23 +8,24 @@ print(userdata[1].split(":")[0], userdata[1].replace("\n", "").split(":")[1])
 
 bot = telebot.TeleBot('6995041657:AAH7KS37WX2mpGrURdejFsyDQ6G_EayUJUA')
 
-
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, 'Hello! To continue working, you need to log in')
-    bot.send_message(message.chat.id, '<b>Login:</b>', parse_mode='html')
-
+    bot.send_message(message.chat.id, 'Приветствуем Вас в сервисе онлайн мониторинга'
+                                      ' <i>уязвимостей</i>. \nДля регистрации введите /reg' , parse_mode='html')
+@bot.message_handler(commands=['reg'])
+def handle_reg(message):
+    bot.send_message(message.chat.id, 'Для продолжения необходимо авторизироваться')
+    bot.send_message(message.chat.id, '<b>Логин:</b>', parse_mode='html')
 
 name = None
 password = None
 login_check = 0
 
-
 @bot.message_handler(content_types=['text'])
 def user_login(message):
     global name
     name = message.text.strip()
-    bot.send_message(message.chat.id, '<b>Password:</b>', parse_mode='html')
+    bot.send_message(message.chat.id, '<b>Пароль:</b>', parse_mode='html')
     bot.register_next_step_handler(message, user_password)
 
 
@@ -33,10 +35,22 @@ def user_password(message):
     password = message.text.strip()
     for i in range(len(userdata)):
         if (name == userdata[i].split(":")[0]) and (password == userdata[i].replace("\n", "").split(":")[1]):
-            bot.send_message(message.chat.id,'<b>МЫ ВОШЛИ</b>', parse_mode='html' )
+            bot.send_message(message.chat.id,'<b>Вы вошли</b>', parse_mode='html' )
             login_check = 1
             break
     if login_check == 0:
         bot.send_message(message.chat.id, '<b>Данные неверные</b>', parse_mode='html')
+        handle_reg(message)
 
+def delog(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Делогин")
+    markup.add(btn1)
+
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if(message.text == "Делогин"):
+        bot.send_message(message.chat.id, text="Вы разлогинились")
+        login_check = 0
+        handle_reg(message)
 bot.infinity_polling()
